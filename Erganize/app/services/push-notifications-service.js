499e9ec50f516604917b2a2f7447d5f91361416b
Rosen Vladimirov <rosen.vladimirov@telerik.com>
2016-06-13 14:14:44 +0000

    Fix push notifications

--- Erganize/app/services/push-notifications-service.js
+++ Erganize/app/services/push-notifications-service.js
@@ -11,33 +11,23 @@ var pushSettings = {
     android: {
         projectNumber: googleApiProjectNumber
     },
-    wp8: {
-        channelName: 'EverlivePushChannel'
-    },
     notificationCallbackIOS: function(e) {
         // logic for handling push in iOS
     },
     notificationCallbackAndroid: function(e) {
         console.log('notification call back android called');
-    },
-    notificationCallbackWP8: function(e) {
-        // logic for handling push in Windows Phone. Not available in NativeScript.
-    },
-    customParameters: {
-        myParameter1: "MyValue1",
-        myParameter2: "MyValue2"
     }
 };
 
 el.push.register(
     pushSettings, 
     function successCallback(data) {
-        console.log("SUCCESS!!!");
         // This function will be called once the device is successfully registered
+        console.log("SUCCESS!!!");
     },
     function errorCallback(error) {
-        console.log("Error, " + error.message);
         // This callback will be called any errors occurred during the device
         // registration process
+        console.log("Error, " + error.message);
     }
 );
