0a4bce7623d4dbc0bf80f2aa997db5ca7613430b
Pavel Kolev <pavel.kolev@telerik.com>
2016-06-13 07:37:30 +0000

    Changes made by Pavel Kolev (pavel.kolev@telerik.com).

--- PPP/app/app.js
+++ PPP/app/app.js
@@ -1,10 +1,5 @@
-var application = require('application'),
-    mainModule = 'navigation/navigation';
+var application = require('application');
 
-// START_CUSTOM_CODE_nativeScriptApp
-// Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
-
-// END_CUSTOM_CODE_nativeScriptApp
 application.start({
     moduleName: mainModule
 });
