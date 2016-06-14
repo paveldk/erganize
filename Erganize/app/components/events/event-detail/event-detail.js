4de86855e18bf3c2fc1bfb7242ffc693dccd7bb1
Garo Garabedian <garo.garabedian@telerik.com>
2016-06-14 10:35:59 +0000

    Changes made by Garo Garabedian (garo.garabedian@telerik.com).

--- Erganize/app/components/events/event-detail/event-detail.js
+++ Erganize/app/components/events/event-detail/event-detail.js
@@ -1,7 +1,20 @@
+var topmost = require("ui/frame").topmost,
+    page;
+
 function navigatedTo(args) {
-    var page = args.object;
+    page = args.object;
     
     page.bindingContext = page.navigationContext;
 }
 
+function onSettings(args) {
+    page = args.object;
+    
+    topmost().navigate({
+        moduleName: "components/events/event-settings/event-settings",
+        context: page.bindingContext
+    });
+}
+
 exports.navigatedTo = navigatedTo;
+exports.onSettings = onSettings;
